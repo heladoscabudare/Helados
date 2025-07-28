18e07c612c955450b5c9719709b44a542605b10a
heladoscabudare <heladosalmayorcabudare@gmail.com>
2025-07-28 19:50:24 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Configuración de Supabase (usa tus propios datos)
+const SUPABASE_URL = 'https://cgepkgurvcqcyleyorrp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnZXBrZ3VydmNxY3lsZXlvcnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MTg0OTgsImV4cCI6MjA2OTI5NDQ5OH0.VhHC9FEZntG7KiIa6Y__J0rfbsSJVyz7V1dNt6NpwQI';
